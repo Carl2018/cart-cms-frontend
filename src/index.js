c3bae08f18fe163eb7af2693653c5d9d4ec28672
jiahaolai <cruithne_2292@hotmail.com>
2020-07-13 18:37:32 +0800

    feat($rootScope): remove fake backend The following changes have been made: -removed fake backend

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 // setup fake backend
-import { configureFakeBackend } from './_helpers/fake-backend';
-configureFakeBackend();
+//import { configureFakeBackend } from './_helpers/fake-backend';
+//configureFakeBackend();
 
 ReactDOM.render(
   <React.Fragment>
