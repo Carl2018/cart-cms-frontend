495179945e50aa7b6bd6d0786deda261f5f0b752
carside <laikapo2014@gmail.com>
2020-10-13 17:28:36 +0800

    Adjust content height to be at least 100vh in App.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const config = {
-    // apiUrl: 'http://localhost:8080/api',
-    apiUrl: 'https://cms.heymandi.com/api',
+    apiUrl: 'http://localhost:8080/api',
+    // apiUrl: 'https://cms.heymandi.com/api',
 };
--- src/pages/Flag/TableBody.js
+++ src/pages/Flag/TableBody.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import {styles} from './flagRow.css';
 
 /*
 		This component is a table
