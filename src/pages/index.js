b70f452bb70df1642ed3e14212b871c8136319b9
jiabao <laikapo2014@gmail.com>
2021-03-01 14:27:32 +0800

    feat(src/pages/index.js): import components from tag page in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,5 @@ export * from './Stat/Stat';
 export * from './Template/Template';
 export * from './Title/Title';
 export * from './Word/Word';
-export * from './Tag/Tag';
-export * from './Tag/TagList';
+export * from './Tag/CustomizedTag';
 export * from './Tag/TagCandidate';
